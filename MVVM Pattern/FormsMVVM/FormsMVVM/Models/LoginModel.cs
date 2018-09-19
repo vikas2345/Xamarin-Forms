b99d67fa1a159bda76c89796e6fa64f902f6650b
Vikas <21singhvikas@gmail.com>
2018-09-19 15:51:04 +0530

    no message

---
+++ MVVM Pattern/FormsMVVM/FormsMVVM/Models/LoginModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FormsMVVM.Models
+{
+    public class LoginModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public string username;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Username"));
+            }
+        }
+
+        public string password;
+        public string Password { get {return password; }
+            set {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            } }
+
+        public ICommand SubmitCommand { get; set; }
+
+        public LoginModel()
+        {
+            SubmitCommand = new Command(OnSubmit);
+        }
+        
+        public void OnSubmit()
+        {
+            if (!(string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password)))
+            {
+                MessagingCenter.Send(this, "Login Alert", Username);
+            }
+        }
+    }
+}
+
---
+++ MVVM Pattern/FormsMVVM/FormsMVVM/Views/Login.xaml.cs
@@ -0,0 +1,41 @@
+﻿using FormsMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormsMVVM.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+        public LoginModel loginModel;
+
+		public Login ()
+		{
+			InitializeComponent ();
+            loginModel = new LoginModel();
+            MessagingCenter.Subscribe<LoginModel,string>(this, "Login Alert",(sender,username)=>
+            {
+                DisplayAlert("Title",username, "Oke");
+            });
+            this.BindingContext = loginModel;
+
+            EntryUsername.Completed += (object sender, EventArgs e) =>
+            {
+                EntryPassword.Focus();
+            };
+
+            EntryPassword.Completed += (object sender, EventArgs e) =>
+            {
+                loginModel.SubmitCommand.Execute(null);
+            };
+
+        }
+                
+    }
+}
